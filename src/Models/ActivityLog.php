88c8265a750baf4f0d91f7507c9a2942510ac091
Zaid Bin Khalid <9605800+mianzaid@users.noreply.github.com>
2024-04-19 17:22:45 +0900

    Update ActivityLog.php

--- src/Models/ActivityLog.php
+++ src/Models/ActivityLog.php
@@ -7,10 +7,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class ActivityLog extends Model
 {
-
-
     use HasFactory;
     protected $table = 'activity_log_master';
-    // Disable Laravel's mass assignment protection
-    protected $guarded = [];
+    protected $fillable = [
+        'id',
+        'log',
+        'server_ip_detail',
+        'user_ip_detail',
+        'route_detail',
+        'query_string',
+        'user_id',
+        'user',
+        'created_at',
+        'updated_at',
+    ];
 }
