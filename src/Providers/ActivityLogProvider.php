ee87879740da2b5261ab51e475b92170f209037a
Zaid Bin Khalid <9605800+mianzaid@users.noreply.github.com>
2024-04-19 16:58:48 +0900

    Update ActivityLogProvider.php
    add config

--- src/Providers/ActivityLogProvider.php
+++ src/Providers/ActivityLogProvider.php
@@ -12,6 +12,14 @@ class ActivityLogProvider extends ServiceProvider
      *
      * @return void
      */
+
+    // Register the config in our package so we can use
+    public function register()
+    {
+        $this->mergeConfigFrom(__DIR__ . '/../config/config.php', 'lcwActivityLogConfig');
+    }
+
+
     public function boot()
     {
         $this->loadRoutesFrom(__DIR__ . '/../routes/web.php');
@@ -26,6 +34,10 @@ class ActivityLogProvider extends ServiceProvider
                     __DIR__ . '/../database/migrations/create_activity_log_master.php.stub' => database_path('migrations/' . date('Y_m_d_His', time()) . '_create_activity_log_master.php'),
                 ], 'migrations');
             }
+            //Export config
+            $this->publishes([
+                __DIR__ . '/../config/config.php' => config_path('lcw_activity_log_config.php'),
+            ], 'config');
         }
     }
 }
